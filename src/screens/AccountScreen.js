544a3b2a3837affbb72979b814672a636217a468
Yuri da Mata <yuridamata@gmail.com>
2022-06-15 10:38:23 -0300

    navbar fixes

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,7 +4,7 @@ import { Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { SafeAreaView } from "react-navigation";
 import { Context as AuthContext } from "../context/AuthContext";
-
+import { FontAwesome } from "@expo/vector-icons";
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
@@ -17,6 +17,11 @@ const AccountScreen = () => {
       </SafeAreaView>
     </View>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: "Account",
+  tabBarIcon: <FontAwesome name="gear" size={20} />,
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,4 +1,4 @@
-import "../_mockLocation";
+// import "../_mockLocation";
 
 import React, { useContext, useCallback } from "react";
 import { StyleSheet } from "react-native";
@@ -8,6 +8,9 @@ import Map from "../components/Map";
 import useLocation from "../hooks/useLocation";
 import { Context as LocationContext } from "../context/LocationContext";
 import TrackForm from "../components/TrackForm";
+
+import { FontAwesome } from "@expo/vector-icons";
+
 const TrackCreateScreen = ({ isFocused }) => {
   const {
     addLocation,
@@ -32,6 +35,11 @@ const TrackCreateScreen = ({ isFocused }) => {
       <TrackForm />
     </SafeAreaView>
   );
+};
+
+TrackCreateScreen.navigationOptions = {
+  title: "Add Track",
+  tabBarIcon: <FontAwesome name="plus" size={20} />,
 };
 
 export default withNavigationFocus(TrackCreateScreen);

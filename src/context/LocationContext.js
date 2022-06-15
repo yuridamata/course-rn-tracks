754e645de7e0fc798b2a1f4f502a35e830b18979
Yuri da Mata <yuridamata@gmail.com>
2022-06-15 08:50:20 -0300

    feat: reset track create form and navigate to track list

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -12,6 +12,8 @@ const locationReducer = (state, action) => {
       return { ...state, recording: false };
     case "change_name":
       return { ...state, name: action.payload };
+    case "reset":
+      return { ...state, name: "", locations: [] };
     default:
       return state;
   }
@@ -28,8 +30,11 @@ const stopRecording = (dispatch) => () => {
   dispatch({ type: "stop_recording" });
 };
 
-const addLocation = (dispatch) => (location, recording) => {
+const reset = (dispatch) => () => {
+  dispatch({ type: "reset" });
+};
 
+const addLocation = (dispatch) => (location, recording) => {
   dispatch({ type: "add_current_location", payload: location });
   if (recording) {
     dispatch({ type: "add_location", payload: location });
@@ -38,7 +43,7 @@ const addLocation = (dispatch) => (location, recording) => {
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, changeName },
+  { startRecording, stopRecording, addLocation, changeName, reset },
   {
     recording: false,
     locations: [],
